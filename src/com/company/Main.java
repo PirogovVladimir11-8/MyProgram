9096f5ea55aae7c7575866630ab131321e8c3f17
PirogovVladimir11-8 <89213654410d@gmail.com>
2016-11-07 16:43:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+    public static PrintStream out = System.out;
+    public static void main(String[] args) {
+        boolean a;
+        int b,c,d,e;
+        b=in.nextInt();
+        c=in.nextInt();
+        d=in.nextInt();
+        a= ((b%2==0)&&(c%2==0)) ||  ((d%2==0)&&(c%2==0))||  ((d%2==0)&&(b%2==0)) ;
+    out.println(a);}}
+
