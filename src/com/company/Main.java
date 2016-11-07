2db084a92013f7674c3b266cbe612dbdd5af3c5e
PirogovVladimir11-8 <89213654410d@gmail.com>
2016-11-07 16:57:49 +0300

    переписал hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,11 @@ public class Main {
     public static PrintStream out = System.out;
     public static void main(String[] args) {
         boolean a;
-        int b,c,d,e;
-        b=in.nextInt();
-        c=in.nextInt();
-        d=in.nextInt();
-        a= ((b%2==0)&&(c%2==0)) ||  ((d%2==0)&&(c%2==0))||  ((d%2==0)&&(b%2==0)) ;
-    out.println(a);}}
-
+        int b, c, d, e;
+        b = in.nextInt();
+        c = in.nextInt();
+        d = in.nextInt();
+        a = ((b % 2 == 0) && (c % 2 == 0)) || ((d % 2 == 0) && (c % 2 == 0)) || ((d % 2 == 0) && (b % 2 == 0));
+        out.println(a);
+        out.println("Hello world");
+    }}
